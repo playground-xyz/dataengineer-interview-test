24acefd860fd7bc7eb424399e4c453da160cd496
Si Wang <swang2@woolworths.com.au>
2022-11-07 17:29:57 +1100

    house cleaning and add comments

--- application/common/template.py
+++ application/common/template.py
@@ -4,6 +4,10 @@ from jinja2 import Template
 
 class SqlTemplate:
     def get_number_of_words_sql():
+        """Generate Sql statement to get the number of words in the cat fact
+        :param: None
+        :return: sql statement
+        """
         sql = """
                 SELECT
                         COUNT(DISTINCT word) AS num_of_word
@@ -11,7 +15,12 @@ class SqlTemplate:
                 """
         return sql
 
-    def get_nth_common_or_least_unicode(top_nth_code: int = 1, most_command: bool = True):
+    def get_nth_common_or_least_unicode(top_nth_code: int = 1, most_common: bool = True):
+        """Generate rendered sql statement to get the nth most common or least unicode
+        :param: top_nth_code: nth unicode based on count. default is 1
+        :param: most_common: True for most common, False for least common. default is True
+        :return: sql statement
+        """
         sql = """
            WITH sum_unicode AS (
              SELECT
@@ -24,7 +33,7 @@ class SqlTemplate:
            SELECT
                 unicode
                 ,unicode_count
-                ,DENSE_RANK() OVER (ORDER BY unicode_count {% if most_command %} DESC {% endif %}) as _rank
+                ,DENSE_RANK() OVER (ORDER BY unicode_count {% if most_common %} DESC {% endif %}) as _rank
            FROM sum_unicode
            )
 
@@ -34,11 +43,16 @@ class SqlTemplate:
 
         """
         render_sql = Template(sql).render(
-            top_nth_code=top_nth_code, most_command=most_command)
+            top_nth_code=top_nth_code, most_common=most_common)
 
         return render_sql
 
-    def get_top_bottom_n_words(top_nth_word: int = 20, most_command: bool = True):
+    def get_top_bottom_n_words(top_nth_word: int = 20, most_common: bool = True):
+        """Generate rendered sql statement to get the top or bottom n words
+        :param: top_nth_word: nth word based on count. default is 20
+        :param: most_common: True for most common, False for least common. default is True
+        :return: rendered sql statement
+        """
         sql = """
            WITH sum_word_count AS (
              SELECT
@@ -51,7 +65,7 @@ class SqlTemplate:
            SELECT
                 word
                 ,word_count
-                ,RANK() OVER (ORDER BY word_count {% if most_command %} DESC {% endif %}) as _rank
+                ,RANK() OVER (ORDER BY word_count {% if most_common %} DESC {% endif %}) as _rank
            FROM sum_word_count
            )
 
@@ -62,11 +76,17 @@ class SqlTemplate:
 
         """
         render_sql = Template(sql).render(
-            top_nth_word=top_nth_word, most_command=most_command)
+            top_nth_word=top_nth_word, most_common=most_common)
 
         return render_sql
 
-    def get_nth_common_or_least_country(top_nth_country: int = 1, most_command: bool = True):
+    def get_nth_common_or_least_country(top_nth_country: int = 1, most_common: bool = True):
+        """Generate rendered sql statement to get the nth most common or least country
+        :param: top_nth_country: nth country based on count. default is 1
+        :param: most_common: True for most common, False for least common. default is True
+        :return: Rendered sql statement
+        """
+
         sql = """
            WITH sum_word_count AS (
              SELECT
@@ -82,7 +102,7 @@ class SqlTemplate:
            SELECT
                 word
                 ,word_count
-                ,DENSE_RANK() OVER (ORDER BY word_count {% if most_command %} DESC {% endif %}) as _rank
+                ,DENSE_RANK() OVER (ORDER BY word_count {% if most_common %} DESC {% endif %}) as _rank
            FROM sum_word_count
            )
 
@@ -92,6 +112,6 @@ class SqlTemplate:
 
         """
         render_sql = Template(sql).render(
-            top_nth_country=top_nth_country, most_command=most_command)
+            top_nth_country=top_nth_country, most_common=most_common)
 
         return render_sql
--- application/main.py
+++ application/main.py
@@ -22,13 +22,18 @@ current_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def init_db():
+    # Inititalize the database, create cat facts,word count unicode table
+    # Create country table and load data
     try:
         os.remove(current_path+"/"+"dbs/interview.db")
     except OSError:
         pass
 
+    log.info(f"remove database file")
+
     country_data = []
 
+    # Read country data from csv file
     with open(current_path+"/"+"/country.csv") as f:
         reader = csv.DictReader(f)
         for row in reader:
@@ -36,25 +41,30 @@ def init_db():
 
     ddls = [Config.COUNTRY_DDL, Config.CAT_FACT_DDL,
             Config.CAT_FACT_UNICODE_DDL, Config.CAT_FACT_WORDS_DDL]
+    # Create database and tables, load country data
     with sqlite3.connect(current_path+"/"+'dbs/interview.db') as conn:
         cursor = conn.cursor()
         for ddl in ddls:
             cursor.execute(ddl)
         cursor.executemany("INSERT INTO COUNTRIES VALUES(?, ?)", country_data)
 
+    log.info("table created and country data loaded")
+
 
 async def get_fact(url: str, rate, session) -> str:
+    # Get fact data from API asynchronously
     try:
         async with rate:
             async with session.request(method="GET", url=url) as response:
                 data = await response.text()
                 status = response.status
-                log.info(f"Make request: {url}. Status; {status}")
-                # Only making API call in every 1/10 secs
+                if status != 200:
+                    log.warning(
+                        f"Request failed for {url} with status code {status}")
                 await asyncio.sleep(Config.API_REQUEST_INTERVAL)
                 return data
     except Exception as e:
-        print(e)
+        log.warning(e)
 
 
 async def load_fact_data_to_dict():
@@ -70,23 +80,28 @@ async def load_fact_data_to_dict():
     # Store the fact data with Haskey to remove duplication
     facts_dict = defaultdict(lambda: "fact's hashkey not found")
 
-    # Limit 10 requests at same time
+    # Create a semaphore to limit the number of concurrent requests, by default 10
     rate = asyncio.Semaphore(Config.API_RATE_LIMIT)
     async with aiohttp.ClientSession() as session:
         requests = [get_fact(url=url, rate=rate, session=session)
                     for url in urls]
         response_results = await asyncio.gather(*requests)
 
+    # Store the raw response data if result not None
     for result in response_results:
         if result:
             raw_data = json.loads(result)
             if "data" in raw_data.keys():
                 facts_list += raw_data["data"]
 
-    facts_with_key = []
+    # Remove duplication by using fact's hashkey
+    fact_count = 0
     for fact in facts_list:
         facts_dict[hashlib.sha1(fact["fact"].encode(
             "utf-8")).hexdigest()] = fact["fact"]
+        fact_count += 1
+    log.info(
+        f"Total fact count read from `https://catfact.ninja/facts`: {fact_count}")
 
     return facts_dict
 
@@ -115,6 +130,7 @@ def main():
         facts_unicode_data_sql += [(hashkey, char, count)
                                    for char, count in char_counts.items()]
 
+    # convert facts data format to be inserted into sql
     facts_data_sql = [(hashkey, fact)
                       for hashkey, fact in facts_data.items()]
 
@@ -122,28 +138,54 @@ def main():
         cursor = conn.cursor()
         cursor.executemany(
             "INSERT INTO CAT_FACTS VALUES(?, ?)", facts_data_sql)
+        log.info("cat facts data loaded")
         cursor.executemany(
             "INSERT INTO CAT_FACT_WORD_COUNT VALUES(?, ?, ?)", facts_words_data_sql)
+        log.info("cat facts word count data loaded")
         cursor.executemany(
             "INSERT INTO CAT_FACT_UNICODE_COUNT VALUES(?, ?, ?)", facts_unicode_data_sql)
-        cursor.execute("select * from CAT_FACT_UNICODE_COUNT")
+        log.info("cat facts unicode count data loaded")
 
     with sqlite3.connect(current_path+"/"+"dbs/interview.db") as conn:
-        cursor = conn.cursor()
-        cursor.execute(SqlTemplate.get_number_of_words_sql())
-        print(cursor.fetchall())
 
-        cursor.execute(
-            SqlTemplate.get_nth_common_or_least_unicode())
-        print(cursor.fetchall())
+        try:
+            cursor = conn.cursor()
+            cursor.execute(SqlTemplate.get_number_of_words_sql())
+            no_of_words = cursor.fetchall()[0][0]
+            log.info(f"Total number of words: {no_of_words}")
+        except Exception as e:
+            log.warning(e)
 
-        cursor.execute(
-            SqlTemplate.get_top_bottom_n_words())
-        print(cursor.fetchall())
+        try:
+            cursor.execute(
+                SqlTemplate.get_nth_common_or_least_unicode())
+            temp = cursor.fetchall()
+            most_common_unicode = temp[0][0]
+            most_common_unicode_count = temp[0][1]
+            log.info(
+                f"Most common unicode is `{most_common_unicode}` and count is {most_common_unicode_count}")
+        except Exception as e:
+            log.warning(e)
 
-        cursor.execute(
-            SqlTemplate.get_nth_common_or_least_country())
-        print(cursor.fetchall())
+        try:
+            cursor.execute(
+                SqlTemplate.get_top_bottom_n_words(top_nth_word=20, most_common=True))
+            top_20_words = cursor.fetchall()
+            log.info(f"Top 20 words and count: {top_20_words}")
+        except Exception as e:
+            log.warning(e)
+
+        try:
+            cursor.execute(
+                SqlTemplate.get_nth_common_or_least_country())
+            temp = cursor.fetchall()
+            most_common_country = temp[0][0]
+            most_common_country_count = temp[0][1]
+            log.info(
+                f"Most common country is {most_common_country} and count is {most_common_country_count}")
+
+        except Exception as e:
+            log.warning(e)
 
 
 if __name__ == "__main__":
