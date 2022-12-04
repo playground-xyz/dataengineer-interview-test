996811a2d38bae69ef260205b4beede8e2331999
abzeefly <abdullahzayed@gmail.com>
2022-12-04 20:55:21 +1100

    Added more tests

--- zansari_etl_master/test.py
+++ zansari_etl_master/test.py
@@ -43,6 +43,38 @@ target_extendedprice = df.iloc[0]['sum_extendedprice']
 print(source_extendedprice, target_extendedprice)
 assert(int(source_extendedprice) == int(target_extendedprice))
 
+# TEST TOP 5 SUM Extended price by country
+source_query = f"""
+SELECT D.n_name AS name, SUM(A.l_extendedprice) AS sum_extendedprice 
+FROM lineitem A
+LEFT JOIN orders B ON A.l_orderkey = B.o_orderkey
+LEFT JOIN customer C ON C.c_custkey = B.o_custkey
+LEFT JOIN nation D ON D.n_nationkey = C.c_nationkey
+GROUP BY  D.n_name
+ORDER BY SUM(l_extendedprice) DESC
+LIMIT 5
+"""
+df_source = pd.read_sql(source_query, raw_conn)
+
+target_query = f"""
+SELECT D.n_name AS name, SUM(A.extendedprice) AS sum_extendedprice 
+FROM fact_lineitem A
+LEFT JOIN dim_customer C ON C.c_custkey = A.custkey
+LEFT JOIN dim_nation D ON D.n_nationkey = C.c_nationkey
+GROUP BY  D.n_name
+ORDER BY SUM(extendedprice) DESC
+LIMIT 5
+"""
+df_target = pd.read_sql(target_query, semantic_conn)
+
+assert(df_source.iloc[0]['name'] == df_target.iloc[0]['name'])
+assert(df_source.iloc[1]['name'] == df_target.iloc[1]['name'])
+assert(df_source.iloc[2]['name'] == df_target.iloc[2]['name'])
+assert(df_source.iloc[3]['name'] == df_target.iloc[3]['name'])
+assert(df_source.iloc[4]['name'] == df_target.iloc[4]['name'])
+
+# TEST
+print("All Tests Passed !!")
 #..... more tests
 
 
